d4ad4c7da5d549fbb05d97710dcd8cd501f2f147
bradfordlauren <bradfordlauren@yahoo.com>
2016-09-09 12:38:35 -0400

    Lab 8 Complete

--- Lab8_Batting/Program.cs
+++ Lab8_Batting/Program.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 
 namespace Lab8_Batting
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            string userAnswer;
+
+            //write code here
+            Console.WriteLine("Welcome to Batting Average Calculator!\n");
+            do
+            {
+                Console.WriteLine("Enter number of times at bat:");
+                int timesAtBatInput = Convert.ToInt32(Console.ReadLine());
+
+
+                int[] hitResults = new int[timesAtBatInput];
+                int batNumber = 1;
+                for (int i = 0; i < timesAtBatInput; i++)
+
+                {
+                    Console.WriteLine("Enter Your At-Bat Results #" + batNumber + ":");
+                    Console.WriteLine("0=out, 1=single, 2=double, 3=triple, 4=home run\n");
+
+                    hitResults[i] = Convert.ToInt32(Console.ReadLine());
+                    {
+                    }
+                    batNumber++;
+                }
+                Console.WriteLine("Batting Average:" + CalculateBattingAverage(hitResults));
+                Console.WriteLine("Slugging Average:" + CalculateSluggingAverage(hitResults));
+
+                Console.WriteLine("Another Batter? (y/n)\n");
+                userAnswer = Console.ReadLine();
+
+            } while ("y" == userAnswer);
+            Console.WriteLine("Thank You for using our Batting Average Calculator!");
+
+
+
+
         }
+        public static double CalculateBattingAverage(int[] battingSheet)
+        {
+            int hitCounter = 0;
+            for (int i = 0; i < battingSheet.Length; i++)
+
+                if (battingSheet[i] > 0)
+                    hitCounter = hitCounter + 1;
+            return (double)hitCounter / battingSheet.Length;
+        }
+        public static double CalculateSluggingAverage(int[] battingSheet)
+        {
+            int basesSum = battingSheet.Sum();
+
+
+            return (double)basesSum / battingSheet.Length;
+        }
+
+
     }
 }
+
---
+++ Lab8_BattingTest/AverageTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab8_Batting;
+
+namespace Lab8_BattingTest
+{
+    [TestClass]
+    public class AverageTests
+    {
+        [TestMethod]
+        public void TestBattingPercentageis0WhenNoRuns()
+        {
+            int[] battingSheet = new int[5];
+            battingSheet[0] = 0;
+            battingSheet[1] = 0;
+            battingSheet[2] = 0;
+            battingSheet[3] = 0;
+            battingSheet[4] = 0;
+            double battingAverage = Program.CalculateBattingAverage(battingSheet);
+            Assert.AreEqual(0, battingAverage);
+
+        }
+        [TestMethod]
+        public void TestBattingPercentageis60()
+        {
+            int[] battingSheet = new int[5];
+            battingSheet[0] = 0;
+            battingSheet[1] = 0;
+            battingSheet[2] = 2;
+            battingSheet[3] = 4;
+            battingSheet[4] = 1;
+            double battingAverage = Program.CalculateBattingAverage(battingSheet);
+            Assert.AreEqual(0.6, battingAverage);
+        }
+        [TestMethod]
+        public void TestSluggingPercentageis60()
+        {
+            int[] battingSheet = new int[5];
+            battingSheet[0] = 0;
+            battingSheet[1] = 0;
+            battingSheet[2] = 2;
+            battingSheet[3] = 4;
+            battingSheet[4] = 1;
+            double sluggingAverage = Program.CalculateSluggingAverage(battingSheet);
+            Assert.AreEqual(1.4, sluggingAverage);
+        }
+    }
+}
